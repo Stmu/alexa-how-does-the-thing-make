66ee9a06f42602f8ad538a0fb81238d749d1fd48
stefan <stmu@gmx.net>
2017-12-28 16:41:32 +0100

    .

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var Alexa = require("alexa-sdk");
 exports.handler = function (event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
     alexa.appId = APP_ID // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
-    alexa.registerHandlers(newSessionHandlers, startHandler, questionHandler);
+    alexa.registerHandlers(newSessionHandlers, startHandler, questionHandler, helpHandler);
     alexa.execute();
 };
 
